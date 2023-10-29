51a325e71cc884f6a44fc0ad361e07103e5a6746
lirikape <lirikape@gmail.com>
2023-10-29 11:54:05 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,6 @@ import { SearchUser } from './Filter/SearchUser';
 
 export const App = () => {
   const [filter, setFilter] = useState('');
-  // const [contacts, setContacts] = useState([
-  //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  // ]);
   const [contacts, setContacts] = useState(() => {
     const storedContacts = JSON.parse(window.localStorage.getItem('contacts'));
     return (
